adaa8cd03d0fb9fe8a1f49074d99d881826e93f6
simongfxu <xugaofan@gmail.com>
2016-06-09 22:54:45 +0800

    FIX 无法复制粘贴

--- electron/menus/application_darwin.js
+++ electron/menus/application_darwin.js
@@ -29,6 +29,25 @@ export default function(mainWindow) {
       }
     }]
   }, {
+    label: 'Edit',
+    submenu: [{
+      label: 'Cut',
+      accelerator: 'Command+X',
+      selector: 'cut:'
+    }, {
+      label: 'Copy',
+      accelerator: 'Command+C',
+      selector: 'copy:'
+    }, {
+      label: 'Paste',
+      accelerator: 'Command+V',
+      selector: 'paste:'
+    }, {
+      label: 'Select All',
+      accelerator: 'Command+A',
+      selector: 'selectAll:'
+    }]
+  }, {
     label: 'View',
     submenu: [{
       label: 'Reload',
